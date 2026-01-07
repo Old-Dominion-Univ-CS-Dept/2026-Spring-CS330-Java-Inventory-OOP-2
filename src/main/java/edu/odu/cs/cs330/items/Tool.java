70ac5009cee83fc526aae3362644b9ad12ee7931
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2026-01-07 10:13:52 -0500

    Initial Commit

---
+++ src/main/java/edu/odu/cs/cs330/items/Tool.java
@@ -0,0 +1,155 @@
+package edu.odu.cs.cs330.items;
+
+import java.util.Scanner;
+
+/**
+ * This class represents one tool--as found in most video games. This includes
+ * pickaxes and shovels.
+ *
+ * Tools may not be stacked. All Constructors must initialize Item::stackable
+ * to false.
+ */
+@SuppressWarnings({
+    "PMD.BeanMembersShouldSerialize",
+    "PMD.CloneMethodReturnTypeMustMatchClassName",
+    "PMD.CloneThrowsCloneNotSupportedException",
+    "PMD.LawOfDemeter",
+    "PMD.OnlyOneReturn",
+    "PMD.ProperCloneImplementation",
+    "PMD.MethodArgumentCouldBeFinal",
+    "PMD.LocalVariableCouldBeFinal",
+    "PMD.ShortClassName",
+})
+public class Tool extends Equippable {
+    /**
+     * Base operation (e.g., harvest/mine) speed.
+     */
+    protected int speed;
+
+    /**
+     * Default to an unstackable tool with an empty name, zero durability, zero
+     * speed, an empty material name, no modifier, and a modifier level of 1.
+     */
+    public Tool()
+    {
+        super();
+
+        this.speed = 0;
+    }
+
+    /**
+     * Duplicate a Tool.
+     *
+     * @param src Tool to duplicate
+     */
+    public Tool(Tool src)
+    {
+        super();
+
+        this.setName(src.name);
+
+        // Complete this function
+
+    }
+
+    /**
+     * Retrieve tool speed.
+     *
+     * @return how quickly a tool operates
+     */
+    public int getSpeed()
+    {
+        return this.speed;
+    }
+
+    /**
+     * Set tool speed.
+     *
+     * @param spd new speed
+     */
+    public void setSpeed(int spd)
+    {
+        this.speed = spd;
+    }
+
+    @Override
+    public boolean isStackable()
+    {
+        return false;
+    }
+
+    /**
+     * Read tool attributes.
+     */
+    @Override
+    public void read(Scanner snr)
+    {
+        super.name    = snr.next();
+        material      = snr.next();
+
+        // Complete this function
+    }
+
+    /**
+     * Clone--i.e., copy--this Tool.
+     */
+    @Override
+    public Item clone()
+    {
+        Tool cpy = new Tool();
+
+        cpy.setName(this.name);
+
+        // Complete this function
+
+        return cpy;
+    }
+
+    /**
+     * Check for logical equivalence--based on name, material, and modifier.
+     *
+     * @param rhs object for which a comparison is desired
+     */
+    @Override
+    public boolean equals(Object rhs)
+    {
+        if (!(rhs instanceof Tool)) {
+            return false;
+        }
+
+        Tool rhsItem = (Tool) rhs;
+
+        // Complete this function
+        return false;
+    }
+
+    /**
+     * Generate a hash code by adding the name, material, and modifier hash
+     * codes.
+     */
+    @Override
+    public int hashCode()
+    {
+        // Complete this function
+        return -1;
+    }
+
+    /**
+     * *Print* a Tool.
+     */
+    @Override
+    public String toString()
+    {
+        // This is a free implementation of toString... use it as a hint for
+        // Armour and Consumable
+        return String.join(
+            System.lineSeparator(),
+            String.format("  Nme: %s", super.getName()),
+            String.format("  Dur: %s", this.getDurability()),
+            String.format("  Spd: %d", this.getSpeed()),
+            String.format("  Mtl: %s", this.getMaterial()),
+            String.format("  Mdr: %s (Lvl %d)", this.getModifier(), this.getModifierLevel()),
+            ""
+        );
+    }
+}
